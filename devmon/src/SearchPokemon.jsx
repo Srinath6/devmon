a26d016f335b78243bf7b35e1d1a1decf492d0a7
Srinath6 <57441534+Srinath6@users.noreply.github.com>
2023-11-12 22:28:35 +0530

    feat: setup autocomplete component

--- devmon/src/SearchPokemon.jsx
+++ devmon/src/SearchPokemon.jsx
@@ -5,7 +5,11 @@ import {
   Paper,
   Stack,
   TextField,
+  Typography,
 } from "@mui/material";
+import { useQuery } from "@tanstack/react-query";
+import { useCallback, useMemo, useState } from "react";
+
 const top100Films = [
   { label: "The Shawshank Redemption", year: 1994 },
   { label: "The Godfather", year: 1972 },
@@ -13,6 +17,37 @@ const top100Films = [
 ];
 
 export default function SearchPokemon() {
+  const [processedPokemonList, setProcessedPokemonList] = useState([]);
+  const [selectedPokemon, setSelectedPokemon] = useState("bulbasaur");
+  const {
+    isLoading: isPokemonNameDataLoading,
+    error: pokemonNameDataerror,
+    data: pokemonNameData,
+  } = useQuery({
+    queryKey: ["pokemonNamesList"],
+    queryFn: () =>
+      fetch("https://pokeapi.co/api/v2/pokemon?limit=1292").then((res) =>
+        res.json()
+      ),
+  });
+
+  if (pokemonNameDataerror) {
+    return <Typography>Failed to fetch pokemons list</Typography>;
+  }
+
+  const processPokemonData = useMemo(() => {
+    if (pokemonNameData?.results) {
+      const pokemonNames = pokemonNameData.results.map(
+        (pokemon) => pokemon.name
+      );
+      setProcessedPokemonList(pokemonNames);
+    }
+  }, [pokemonNameData]);
+
+  const handlePokemonNameChange = useCallback((event, newValue) => {
+    setSelectedPokemon(newValue);
+  }, []);
+
   return (
     <Paper elevation={0} variant="outlined" sx={{ m: 2 }}>
       <Stack
@@ -25,11 +60,20 @@ export default function SearchPokemon() {
         <Autocomplete
           disablePortal
           id="search-pokemon"
-          options={top100Films}
+          options={processedPokemonList}
           sx={{ width: 300 }}
           renderInput={(params) => <TextField {...params} label="Pokemons" />}
+          loading={isPokemonNameDataLoading}
+          loadingText={"Fetching pokemons"}
+          onChange={handlePokemonNameChange}
+          value={selectedPokemon}
         />
-        <Button variant="contained"> Find </Button>
+        <Button
+          variant="contained"
+          onClick={() => console.log(selectedPokemon)}
+        >
+          Find
+        </Button>
       </Stack>
     </Paper>
   );
